832ccb8502af19817fcca9db0e95d6c26dcc2d1a
Divya Kumari <rounak880013@gmail.com>
2022-01-27 10:17:58 +0530

    two commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ function Contact(){
             <div class="text_2_a">Name</div>
             <input class="Name" type="text" name="name" placeholder="Name"/>
             <div class="text_2">Email</div>
-            <input class="Email" type="Email" name="Email" placeholder="Email"/>
+            <input class="Email" type="Email" name="Email" placeholder="you@company.com"/>
             <div class="text_2">Phone number</div>
             <input class="Phone_number" type="Number" name="Email" placeholder="Phone Number"/>
             <div class="text_2">Message</div>
